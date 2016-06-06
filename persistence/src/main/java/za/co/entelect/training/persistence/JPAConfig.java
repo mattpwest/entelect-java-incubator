61b3cb276df81d270fc1e5309e8c9dfe526aa703
Matt Van Der Westhuizen <mattpwest@gmail.com>
2016-06-06 15:04:07 +0200

    Configuration changes to add Spring Data JPA with Hibernate and an H2 in-memory database.

---
+++ persistence/src/main/java/za/co/entelect/training/persistence/JPAConfig.java
@@ -0,0 +1,95 @@
+package za.co.entelect.training.persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("za.co.entelect.training.persistence")
+public class JPAConfig {
+
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean inMemoryEntityManagerFactory(DataSource dataSource,
+                                                                               JpaVendorAdapter vendorAdapter,
+                                                                               LoadTimeWeaver loadTimeWeaver,
+                                                                               Properties hibernateProperties) {
+
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setLoadTimeWeaver(loadTimeWeaver);
+        em.setJpaProperties(hibernateProperties);
+        em.setPackagesToScan("za.co.entelect.training.domain");
+        return em;
+    }
+
+    @Bean
+    public DataSource dataSourceInMemory() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=FALSE");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter vendorAdapter() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
+        return vendorAdapter;
+    }
+
+    @Bean
+    public LoadTimeWeaver loadTimeWeaver() {
+        return new InstrumentationLoadTimeWeaver();
+    }
+
+    @Bean
+    Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.import_files", "dev_initial_data.sql");
+        properties.setProperty("hibernate.hbm2ddl.import_files_sql_extractor", "org.hibernate.tool.hbm2ddl.MultipleLinesSqlCommandExtractor");
+        return properties;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf, DataSource dataSource) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
+
+    @Bean(initMethod="start", destroyMethod="stop")
+    public org.h2.tools.Server h2WebServer() throws SQLException {
+        return org.h2.tools.Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082");
+    }
+
+    @Bean(initMethod="start", destroyMethod="stop")
+    @DependsOn(value = "h2WebServer")
+    public org.h2.tools.Server h2Server() throws SQLException {
+        return org.h2.tools.Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+}
---
+++ persistence/src/main/resources/dev_initial_data.sql
@@ -0,0 +1,1 @@
+INSERT INTO User(Id, Email, Password, FirstName, LastName) VALUES (1, 'admin@entelect.co.za', 'test213', 'Ser', 'Admin');
